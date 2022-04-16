1ba1c1499f45da676ed4f46d1b836a389cae0a72
Garrett Kunde <34901900+gkunde@users.noreply.github.com>
2022-04-16 14:32:03 -0500

    Fix name collision
    A name collision with "delimiter" in the function definition was present. Escaped the keyword to avoid collision.

--- src/reverse_delimited_string.sql
+++ src/reverse_delimited_string.sql
@@ -1,18 +1,17 @@
 CREATE FUNCTION `reverse_delimited_string`(
-	delimited_string VARCHAR(255),
-	delimiter VARCHAR(5)
+	`delimited_string` VARCHAR(255),
+	`delimiter` VARCHAR(5)
 ) RETURNS varchar(255) CHARSET utf8mb4
-    DETERMINISTIC
 BEGIN
-	DECLARE reversed_delimited_string VARCHAR(255) DEFAULT '';
+	DECLARE `reversed_delimited_string` VARCHAR(255) DEFAULT '';
 
-	WHILE LOCATE(delimiter, delimited_string) > 0 DO
-		SET reversed_delimited_string = CONCAT(SUBSTRING_INDEX(delimited_string, delimiter, 1), delimiter, reversed_delimited_string);
-		SET delimited_string = SUBSTR(delimited_string, LOCATE(delimiter, delimited_string) + LENGTH(delimiter));
+	WHILE LOCATE(`delimiter`, `delimited_string`) > 0 DO
+		SET `reversed_delimited_string` = CONCAT(SUBSTRING_INDEX(`delimited_string`, `delimiter`, 1), `delimiter`, `reversed_delimited_string`);
+		SET `delimited_string` = SUBSTR(`delimited_string`, LOCATE(`delimiter`, `delimited_string`) + LENGTH(`delimiter`));
 	END WHILE;
 
-	SET reversed_delimited_string = CONCAT(delimited_string, delimiter, reversed_delimited_string);
-	SET reversed_delimited_string = SUBSTR(reversed_delimited_string, 1, LENGTH(reversed_delimited_string) - 1);
+	SET `reversed_delimited_string` = CONCAT(`delimited_string`, `delimiter`, `reversed_delimited_string`);
+	SET `reversed_delimited_string` = SUBSTR(`reversed_delimited_string`, 1, LENGTH(`reversed_delimited_string`) - 1);
 
-	RETURN reversed_delimited_string;
+	RETURN `reversed_delimited_string`;
 END
