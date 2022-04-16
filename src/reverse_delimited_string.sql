5532b61a5fe36f64d57686f17204a57629f3d42c
Garrett Kunde <34901900+gkunde@users.noreply.github.com>
2022-04-16 13:54:21 -0500

    Create reverse_delimited_string.sql

---
+++ src/reverse_delimited_string.sql
@@ -0,0 +1,17 @@
+CREATE FUNCTION `reverse_sequence_order`(
+	delimited_string VARCHAR(255),
+	delimiter VARCHAR(5)
+) RETURNS VARCHAR(255) DETERMINISTIC
+BEGIN
+	DECLARE reversed_delimited_string VARCHAR(255) DEFAULT '';
+
+	WHILE LOCATE(delimiter, delimited_string) > 0 DO
+		SET reversed_delimited_string = CONCAT(SUBSTRING_INDEX(delimited_string, delimiter, 1), delimiter, reversed_delimited_string);
+		SET delimited_string = SUBSTR(delimited_string, LOCATE(delimiter, delimited_string) + LENGTH(delimiter));
+	END WHILE;
+
+	SET reversed_delimited_string = CONCAT(delimited_string, delimiter, reversed_delimited_string);
+	SET reversed_delimited_string = SUBSTR(reversed_delimited_string, 1, LENGTH(reversed_delimited_string) - 1);
+
+	RETURN reversed_delimited_string;
+END
